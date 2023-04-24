d756c95d7c9fb49aadbcb1b1c5bffd380c968d6a
YeiserCode <131619978+YeiserCode@users.noreply.github.com>
2023-04-24 18:09:11 -0500

    Cambios y Mejoras

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -6,7 +6,10 @@ const Carrito = ({ carrito, eliminarDelCarrito }) => {
       <h2>Carrito de compras</h2>
       {carrito.map((producto) => (
         <div key={producto.id}>
-          <p>{producto.nombre}</p>
+          {/* Mostrar la cantidad de productos junto al nombre del producto */}
+          <p>
+            {producto.nombre} - Cantidad: {producto.cantidad}
+          </p>
           <button onClick={() => eliminarDelCarrito(producto)}>Eliminar</button>
         </div>
       ))}
