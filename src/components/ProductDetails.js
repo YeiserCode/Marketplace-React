f63365110dd9025475cb767bf134554455615f3f
YeiserCode <131619978+YeiserCode@users.noreply.github.com>
2023-04-24 02:18:37 -0500

    Mejorar Detalles de Producto Con Funcion de Lupa

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -2,14 +2,58 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
-import { Container, Typography, Box, Paper, Dialog, DialogContent, IconButton, Grid } from '@mui/material';
-import { Close as CloseIcon } from '@mui/icons-material';
+import { Container, Typography, Box, Paper, Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import ReactImageMagnify from 'react-image-magnify';
+
+const useStyles = makeStyles({
+  mainImage: {
+    width: '100%',
+    height: '300px',
+    objectFit: 'contain',
+    cursor: 'pointer',
+    borderRadius: '4px',
+  },
+  magnifierContainer: {
+    position: 'relative',
+    width: '100%',
+    paddingBottom: '100%',
+    overflow: 'visible',
+  },
+  imageThumbnail: {
+    width: '100%',
+    height: '100px',
+    objectFit: 'cover',
+    cursor: 'pointer',
+    borderRadius: '4px',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 8,
+    right: 8,
+  },
+  paper: {
+    padding: 3,
+    borderRadius: '4px',
+    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.1)',
+  },
+  productInfo: {
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
+  price: {
+    color: '#F15A24',
+  },
+  descriptionTitle: {
+    fontWeight: 'bold',
+  },
+});
 
 const ProductDetails = () => {
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
-  const [open, setOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+  const classes = useStyles();
 
   useEffect(() => {
     const getProductDetails = async () => {
@@ -26,38 +70,59 @@ const ProductDetails = () => {
     getProductDetails();
   }, [productId]);
 
-  const handleClickOpen = (imageUrl) => {
+  const handleImageChange = (imageUrl) => {
     setSelectedImage(imageUrl);
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  }; 
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       {product ? (
         <Box mt={3}>
-          <Paper elevation={3} sx={{ padding: 3 }}>
-            <img
-              src={product.imagenPortada}
-              alt={product.nombre}
-              style={{ width: '100%', height: 'auto', objectFit: 'contain', cursor: 'pointer' }}
-              onClick={() => handleClickOpen(product.imagenPortada)}
-            />
-            <Typography variant="h4">{product.nombre}</Typography>
-            <Typography variant="body1">Precio: ${product.precio}</Typography>
-            <Typography variant="body1">Descripción: {product.descripcion}</Typography>
+          <Paper elevation={3} className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+              <div className={classes.magnifierContainer}>
+                <ReactImageMagnify
+                  {...{
+                    smallImage: {
+                      alt: product.nombre,
+                      isFluidWidth: true,
+                      src: selectedImage || product.imagenPortada,
+                    },
+                    largeImage: {
+                      src: selectedImage || product.imagenPortada,
+                      width: 900,
+                      height: 1150,
+                    },
+                    enlargedImagePosition: 'beside',
+                    enlargedImageContainerDimensions: {
+                      width: '105%',
+                      height: '100%',
+                    },
+                    lensStyle: { backgroundColor: 'rgba(0,0,0,.6)' },
+                  }}
+                  className={classes.mainImage}
+                />
+                  </div>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Box className={classes.productInfo}>
+                  <Typography variant="h4" gutterBottom>{product.nombre}</Typography>
+                  <Typography variant="h6" gutterBottom className={classes.price}>Precio: ${product.precio}</Typography>
+                  <Typography variant="body1" gutterBottom className={classes.descriptionTitle}>Descripción:</Typography>
+                  <Typography variant="body1" gutterBottom>{product.descripcion}</Typography>
+                </Box>
+              </Grid>
+            </Grid>
             {product.imagenes && (
               <Grid container spacing={1} justifyContent="center">
                 {product.imagenes.map((imagen, index) => (
-                  <Grid item key={index} xs={4}>
+                  <Grid item key={index} xs={6} sm={3}>
                     <img
                       src={imagen}
                       alt={`Imagen ${index + 1}`}
-                      style={{ width: '100%', height: '100px', objectFit: 'cover', cursor: 'pointer' }}
-                      onClick={() => handleClickOpen(imagen)}
+                      className={classes.imageThumbnail}
+                      onClick={() => handleImageChange(imagen)}
                     />
                   </Grid>
                 ))}
@@ -69,14 +134,6 @@ const ProductDetails = () => {
         <Typography variant="h5">Cargando detalles del producto...</Typography>
       )}
 
-      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-        <DialogContent>
-          <IconButton edge="end" color="inherit" onClick={handleClose} aria-label="close" sx={{ position: 'absolute', top: 8, right: 8 }}>
-            <CloseIcon />
-          </IconButton>
-          <img src={selectedImage} alt="Selected" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
-        </DialogContent>
-      </Dialog>
     </Container>
   );
 };
