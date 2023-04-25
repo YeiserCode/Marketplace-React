ae7a7943217fef6bbff44914f7c38a2c2d6f24c2
YeiserCode <131619978+YeiserCode@users.noreply.github.com>
2023-04-25 15:06:30 -0500

    Mejoras de diseño

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardContent, CardMedia } from '@mui/material';
 
 const Carrito = ({ carrito, eliminarDelCarrito }) => {
   return (
-    <div className="carrito">
-      <h2>Carrito de compras</h2>
+    <Box display="flex" flexDirection="column" alignItems="center" p={3}>
+      <Typography variant="h4">Carrito de compras</Typography>
       {carrito.map((producto) => (
-        <div key={producto.id}>
-          <p>{producto.nombre}</p>
-          <button onClick={() => eliminarDelCarrito(producto)}>Eliminar</button>
-        </div>
+        <Card key={producto.id} sx={{ maxWidth: 600, minWidth: 300, mt: 2, p: 1, boxShadow: 3 }}>
+          <Box display="flex" flexDirection="row">
+            <CardMedia
+              component="img"
+              sx={{ width: 100, height: 100, objectFit: 'cover' }}
+              image={producto.imagenes?.[0] || ''}
+              alt={producto.nombre}
+            />
+            <CardContent>
+              <Typography variant="h6">{producto.nombre}</Typography>
+              <Box mt={2} display="flex" justifyContent="flex-end">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => eliminarDelCarrito(producto)}
+                  sx={{ transition: 'all 0.3s' }}
+                >
+                  Eliminar
+                </Button>
+              </Box>
+            </CardContent>
+          </Box>
+        </Card>
       ))}
-    </div>
+    </Box>
   );
 };
 
--- src/components/CarritoItemAgregado.js
+++ src/components/CarritoItemAgregado.js
@@ -1,29 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Card, CardContent, CardMedia } from '@mui/material';
 
 const CarritoItemAgregado = ({ findProductById }) => {
   const { productId } = useParams();
   const [producto, setProducto] = useState(null);
 
   useEffect(() => {
-    setProducto(findProductById(productId));
+    const product = findProductById(productId);
+    setProducto(product);
   }, [productId, findProductById]);
 
   if (!producto) {
     return <div>Cargando...</div>;
   }
 
+  const imageUrl = producto.imagenes?.[0] || '';
+
   return (
-    <Box>
-      <Typography variant="h5">Producto agregado al carrito</Typography>
-      <Typography variant="h6">{producto.nombre}</Typography>
-      <Typography variant="body1">{producto.descripcion}</Typography>
-      <Box mt={2}>
-        <Button variant="contained" color="primary" component={Link} to="/carrito">
-          Ver carrito
-        </Button>
-      </Box>
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="calc(100vh - 64px - 120px)">
+      <Card sx={{ maxWidth: 600, minWidth: 300, p: 2, boxShadow: 3 }}>
+        <CardMedia
+          component="img"
+          sx={{ aspectRatio: '16/9' }}
+          image={imageUrl}
+          alt={producto.nombre}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Producto agregado al carrito
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ mt: 1 }}>
+            {producto.nombre}
+          </Typography>
+          <Typography variant="body1" sx={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3, overflow: 'hidden', mt: 1 }}>
+            {producto.descripcion}
+          </Typography>
+        </CardContent>
+        <Box mt={2} display="flex" justifyContent="center">
+          <Button variant="contained" color="primary" component={Link} to="/carrito" sx={{ transition: 'all 0.3s' }}>
+            Ver carrito
+          </Button>
+        </Box>
+      </Card>
     </Box>
   );
 };
