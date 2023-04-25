fa2694a5cb237cf47b373a538df8f13113e6ef20
YeiserCode <131619978+YeiserCode@users.noreply.github.com>
2023-04-25 01:32:13 -0500

    Mejorar Diseño de Inicio de sesion

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,101 @@
 import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  CircularProgress,
+  Box,
+} from '@mui/material';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setMessage('');
+
+    if (!email || !password) {
+      setMessage('Por favor, completa todos los campos.');
+      setLoading(false);
+      return;
+    }
+
     const auth = getAuth();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      console.log('Inicio de sesión exitoso');
+      setMessage('Inicio de sesión exitoso');
     } catch (error) {
+      if (error.code === 'auth/user-not-found') {
+        setMessage('El correo electrónico no está registrado.');
+      } else if (error.code === 'auth/wrong-password') {
+        setMessage('La contraseña es incorrecta.');
+      } else {
+        setMessage('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+      }
       console.error('Error al iniciar sesión:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Iniciar sesión
+      </Typography>
       <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Iniciar sesión</button>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={8} md={6}>
+            <TextField
+              label="Correo electrónico"
+              variant="outlined"
+              fullWidth
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={8} md={6}>
+            <TextField
+              label="Contraseña"
+              variant="outlined"
+              fullWidth
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="center">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={loading}
+              >
+                {loading ? <CircularProgress size={24} /> : 'Iniciar sesión'}
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+      {message && (
+        <Typography
+          variant="subtitle1"
+          color={message.startsWith('Error') ? 'error' : 'success'}
+          style={{ marginTop: '1rem', textAlign: 'center' }}
+        >
+          {message}
+        </Typography>
+      )}
+    </Container>
   );
 };
 
